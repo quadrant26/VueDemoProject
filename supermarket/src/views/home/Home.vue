a3f2956ddc04b1aa35654abc9268d9397f9fb305
quadrant26 <quadrant26@163.com>
2020-01-15 16:19:40 +0800

    顶部导航组件

--- supermarket/src/views/home/Home.vue
+++ supermarket/src/views/home/Home.vue
@@ -1,15 +1,24 @@
 <template>
-    <div>
-      <h2>首页</h2>
-    </div>
+  <div>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import NavBar from 'components/common/navbar/NavBar'
+  export default {
+    name: "Home",
+    components: {
+      NavBar
     }
+  }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    text-align: center;
+    background-color: var(--color-tint);
+  }
 </style>
