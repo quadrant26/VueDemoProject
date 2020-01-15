e4fe95faef02f2e39f8a0368e93350a380222ebf
quadrant26 <quadrant26@163.com>
2020-01-15 15:58:19 +0800

    底部导航组价修改

--- supermarket/src/router/index.js
+++ supermarket/src/router/index.js
@@ -4,10 +4,10 @@ import VueRouter from 'vue-router'
 // 1. 安装插件
 Vue.use(VueRouter)
 
-const Home = () => import('views/home/Home')
-const Category = () => import('views/category/Category')
-const Cart = () => import('views/cart/Cart')
-const Profile = () => import('views/profile/Profile')
+const Home = () => import('views/home/Home');
+const Category = () => import('views/category/Cagegory');
+const Cart = () => import('views/cart/Cart');
+const Profile = () => import('views/profile/Profile');
 
 // 2. 创建 router
 const routes = [
