ee05802174008ea94438ceff3b554a705e409124
quadrant26 <quadrant26@163.com>
2020-01-15 18:06:51 +0800

    本周流行和顶部固定

--- supermarket/src/views/home/Home.vue
+++ supermarket/src/views/home/Home.vue
@@ -1,29 +1,43 @@
 <template>
-  <div>
+  <div id="home">
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <swiper>
-      <swiper-item v-for="item in banners">
-        <a :href="item.link">
-          <img :src="item.image" alt="">
-        </a>
-      </swiper-item>
-    </swiper>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend-view :recommends="recommends"></home-recommend-view>
+    <feature-view></feature-view>
+
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+    </ul>
+
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
   import {getHomeMultidata} from "network/home";
-  import {Swiper, SwiperItem} from 'components/common/swiper'
+
+  import HomeSwiper from './childComponents/HomeSwiper'
+  import HomeRecommendView from './childComponents/HomeRecommendView'
+  import FeatureView from './childComponents/FeatureView'
 
   export default {
     name: "Home",
     components: {
       NavBar,
-      Swiper,
-      SwiperItem
+      HomeSwiper,
+      HomeRecommendView,
+      FeatureView
     },
     data (){
       return {
@@ -43,8 +57,17 @@
 </script>
 
 <style scoped>
+  #home{
+    padding-bottom: 44px;
+  }
   .home-nav{
     text-align: center;
     background-color: var(--color-tint);
+    color:#fff;
+
+    position: fixed;
+    left:0;
+    right:0;
+    z-index: 10;
   }
 </style>
