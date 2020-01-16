a2048f74183a903a69ded70a2d397e761482983c
quadrant26 <quadrant26@163.com>
2020-01-16 13:51:57 +0800

    cnpm install vue-awesome-swiper --save

--- supermarket/src/main.js
+++ supermarket/src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 
 import router from './router/index'
 
+// 这是全局挂载
+/**
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// require styles
+import 'swiper/dist/css/swiper.css'
+// Vue.use(VueAwesomeSwiper, { default global options } )
+Vue.use(VueAwesomeSwiper)
+*/
+
 Vue.config.productionTip = false
 
 new Vue({
